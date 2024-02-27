efa0232f6bd7bffa527d7e0d3a270e44f86ed0d7
jbraitsch <107515919+jbraitsch@users.noreply.github.com>
2024-02-27 04:58:46 +0000

    more testing

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,7 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+<<<<<<< HEAD
+=======
 
+>>>>>>> 7f466c6 (Initial commit)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -37,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+<<<<<<< HEAD
+    'mh_tracker',
+=======
+>>>>>>> 7f466c6 (Initial commit)
 ]
 
 MIDDLEWARE = [
@@ -69,7 +76,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
+<<<<<<< HEAD
+=======
 
+>>>>>>> 7f466c6 (Initial commit)
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -80,12 +90,34 @@ DATABASES = {
     }
 }
 
+<<<<<<< HEAD
+=======
 
+>>>>>>> 7f466c6 (Initial commit)
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
+<<<<<<< HEAD
+        'NAME':
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME':
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME':
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME':
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+=======
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
@@ -100,6 +132,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+>>>>>>> 7f466c6 (Initial commit)
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -111,7 +144,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+<<<<<<< HEAD
+=======
 
+>>>>>>> 7f466c6 (Initial commit)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
