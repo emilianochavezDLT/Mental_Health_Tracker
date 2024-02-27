9d344103ca5c7282650af2683eb2ed2378815f9f
Mercury <83285233+MercRetro@users.noreply.github.com>
2024-02-27 05:06:17 +0000

    more additions

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,16 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mh_tracker.urls')),
-=======
-from django.urls import path
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
->>>>>>> 7f466c6 (Initial commit)
 ]
