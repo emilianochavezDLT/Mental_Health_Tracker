e3f8d0aa969c81238e0279ff88f02dfa53090429
Jack M <6274662-JackM8@users.noreply.replit.com>
2024-03-10 20:51:26 +0000

    Updated JournalEntry model changing excercise to exercise and journal submission now returns the values of the buttons and journal text. Can't save object yet because of User system

--- mh_tracker/models.py
+++ mh_tracker/models.py
@@ -1,24 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
+
 
 #Captures information from user
 class JournalEntry(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE) #Links to a specfic user
-  date_created = models.DateTimeField(auto_now_add=True) #Date and time of entry
-  mood_level = models.IntegerField() # 1 to 5 
-  sleep_quality = models.IntegerField() #1 to 5
-  excercise_time = models.IntegerField() # in minutes
-  diet_quality = models.IntegerField() #1 to 5
-  water_intake = models.IntegerField() #1 to 5 
-  journal_text = models.TextField() #Keep track of journal entry texts
+  user = models.ForeignKey(User,
+                           on_delete=models.CASCADE)  #Links to a specfic user
+  date_created = models.DateTimeField(
+      auto_now_add=True)  #Date and time of entry
+  mood_level = models.IntegerField()  # 1 to 5
+  sleep_quality = models.IntegerField()  #1 to 5
+  exercise_time = models.IntegerField()  # in minutes
+  diet_quality = models.IntegerField()  #1 to 5
+  water_intake = models.IntegerField()  #1 to 5
+  journal_text = models.TextField()  #Keep track of journal entry texts
 
   #Return time created by user
   def __str__(self):
     return f"Journal Entry by {self.user.username} on {self.date_created.strftime('%m/%d/%Y')}"
-  
-
-
-
--- mh_tracker/views.py
+++ mh_tracker/views.py
@@ -1,18 +1,30 @@
 from django.shortcuts import render, redirect
+from mh_tracker.models import JournalEntry, User
 
 
 # Create your views here.
 def home(request):
   return render(request, 'mh_tracker/home.html')
 
+
 #User can long in their journal entry
 def journal_entry(request):
+  #user = User.objects.get(pk=pk)
   if request.method == 'POST':
     #Redirect to the homepage
+    journal_Entry = JournalEntry(user_id=request.user,
+                                 mood_level=request.POST['mood_level'],
+                                 sleep_quality=request.POST['sleep_quality'],
+                                 exercise_time=request.POST['exercise_time'],
+                                 diet_quality=request.POST['diet_quality'],
+                                 water_intake=request.POST['water_intake'],
+                                 journal_text=request.POST['journal_text'])
+    #JournalEntry.save(journal_Entry)
     return redirect('home')
   else:
     #Render the form
     return render(request, 'mh_tracker/journal_entry.html')
+
 
 def color_calendar(request):
   if request.method == 'POST':
@@ -22,6 +34,7 @@ def color_calendar(request):
     #Render the form
     return render(request, 'mh_tracker/color_calender.html')
 
+
 def settings(request):
   if request.method == 'POST':
     #Redirect to the homepage
@@ -29,14 +42,16 @@ def settings(request):
   else:
     #Render the form
     return render(request, 'mh_tracker/settings.html')
-    
+
+
 def login(request):
-      if request.method == 'POST':
-        #Redirect to the homepage
-        return redirect('home')
-      else:
-        #Render the form
-        return render(request, 'mh_tracker/login.html')
+  if request.method == 'POST':
+    #Redirect to the homepage
+    return redirect('home')
+  else:
+    #Render the form
+    return render(request, 'mh_tracker/login.html')
+
 
 def signup(request):
   if request.method == 'POST':
@@ -45,6 +60,7 @@ def signup(request):
   else:
     #Render the form
     return render(request, 'mh_tracker/signup.html')
+
 
 def analytics(request):
   if request.method == 'POST':
