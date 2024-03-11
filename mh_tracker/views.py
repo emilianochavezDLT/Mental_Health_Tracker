30c639b16167e2de78f68c054d9eb4f4a79e1709
Jack M <6274662-JackM8@users.noreply.replit.com>
2024-03-11 01:46:01 +0000

    Submit only available with every radio button group selected and has a default user of 1 currently

--- mh_tracker/views.py
+++ mh_tracker/views.py
@@ -12,14 +12,16 @@ def journal_entry(request):
   #user = User.objects.get(pk=pk)
   if request.method == 'POST':
     #Redirect to the homepage
-    journal_Entry = JournalEntry(user_id=request.user,
-                                 mood_level=request.POST['mood_level'],
-                                 sleep_quality=request.POST['sleep_quality'],
-                                 exercise_time=request.POST['exercise_time'],
-                                 diet_quality=request.POST['diet_quality'],
-                                 water_intake=request.POST['water_intake'],
-                                 journal_text=request.POST['journal_text'])
-    #JournalEntry.save(journal_Entry)
+    journal_Entry = JournalEntry(
+        user_id=1,
+        #user_id=request.user.id,
+        mood_level=request.POST['mood_level'],
+        sleep_quality=request.POST['sleep_quality'],
+        exercise_time=request.POST['exercise_time'],
+        diet_quality=request.POST['diet_quality'],
+        water_intake=request.POST['water_intake'],
+        journal_text=request.POST['journal_text'])
+    JournalEntry.save(journal_Entry)
     return redirect('home')
   else:
     #Render the form
