a97e5d8635328cf2d7c9eeb997bd47098c37c0b4
Kenyou Teoh <137188714+Iforgotaboutitin2seconds@users.noreply.github.com>
2024-03-12 03:20:19 +0000

    formatted urls

--- mh_tracker/urls.py
+++ mh_tracker/urls.py
@@ -4,14 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('journal-entry/', views.journal_entry, name='journal_entry'), 
+    path('journal-entry/', views.journal_entry, name='journal_entry'),
     path('color_calendar/', views.color_calendar, name='color_calendar'),
-    path('settings/', views.settings, name='settings'), 
-    path('login/', views.user_login, name='login'), 
-    path('logout/', views.user_logout, name='logout'), 
-    path('signup/', views.user_signup, name='signup'), 
-    path('analytics/', views.analytics, name='analytics'), 
-  path('get_journal_entries/', views.get_journal_entries, name='get_journal_entries'),
-  
-  
+    path('settings/', views.settings, name='settings'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('signup/', views.user_signup, name='signup'),
+    path('analytics/', views.analytics, name='analytics'),
+    path('get_journal_entries/',
+         views.get_journal_entries,
+         name='get_journal_entries'),
 ]
