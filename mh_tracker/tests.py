723d41521937d29c2de3c6571529bcef4bb0cfc0
Kenyou Teoh <137188714+Iforgotaboutitin2seconds@users.noreply.github.com>
2024-03-13 05:11:57 +0000

    Created some basic tests about models and urls

--- mh_tracker/tests.py
+++ mh_tracker/tests.py
@@ -1,3 +1,70 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import JournalEntry, SubstanceAbuseTracking, Article, Videos
+from datetime import datetime
 
-# Create your tests here.
+
+class ModelsTestCase(TestCase):
+
+  def setUp(self):
+
+    self.user = User.objects.create_user(username='testuser', password='12345')
+
+    self.journal_entry = JournalEntry.objects.create(
+        user=self.user,
+        date_created=datetime.now(),
+        mood_level=5,
+        sleep_quality=4,
+        exercise_time=30,
+        diet_quality=3,
+        water_intake=7,
+        journal_text="Test entry")
+
+    self.substance_abuse_tracking = SubstanceAbuseTracking.objects.create(
+        user=self.user, days_sober=10, counter=2)
+
+    self.article = Article.objects.create(title="Test Article",
+                                          description="A test article.",
+                                          pdf="path/to/pdf")
+
+    self.video = Videos.objects.create(title="Test Video",
+                                       description="A test video.",
+                                       video_link="https://example.com")
+
+  def test_journal_entry_creation(self):
+    self.assertEqual(self.journal_entry.mood_level, 5)
+
+  def test_substance_abuse_tracking_creation(self):
+    self.assertEqual(self.substance_abuse_tracking.days_sober, 10)
+
+  def test_article_creation(self):
+    self.assertEqual(self.article.title, "Test Article")
+
+  def test_video_creation(self):
+    self.assertEqual(self.video.description, "A test video.")
+
+
+class URLAccessTestCase(TestCase):
+
+  def setUp(self):
+
+    self.client = Client()
+
+    self.user = User.objects.create_user(username='testuser', password='12345')
+    self.client.login(username='testuser', password='12345')
+
+  def test_home_access(self):
+    response = self.client.get(reverse('home'))
+    self.assertEqual(response.status_code, 200)
+
+  def test_login_access(self):
+    self.client.logout()
+    response = self.client.get(reverse('login'))
+    self.assertEqual(response.status_code, 200)
+
+  def test_protected_url_redirects(self):
+    self.client.logout()
+    response = self.client.get(reverse('journal_entry'))
+
+    self.assertTrue(response.status_code, 302)
