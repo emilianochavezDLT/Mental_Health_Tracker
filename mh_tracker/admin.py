5ff7a3de6f81adcd831658432dc7a70839d11a38
Kenyou Teoh <137188714+Iforgotaboutitin2seconds@users.noreply.github.com>
2024-05-01 03:29:35 +0000

    update admin.py to have all the models shown in admin panel

--- mh_tracker/admin.py
+++ mh_tracker/admin.py
@@ -3,3 +3,7 @@ from .models import *
 
 #Register models
 admin.site.register(JournalEntry)
+admin.site.register(SubstanceAbuseTracking)
+admin.site.register(Article)
+admin.site.register(Videos)
+admin.site.register(Therapist)
